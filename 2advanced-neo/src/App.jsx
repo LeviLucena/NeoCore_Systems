3ee638d774734ed4e9911c82f913bb51b83789d8
Levi Lucena <sr_lucen@yahoo.com.br>
2025-06-02 19:06:46 -0300

    Update App.jsx

--- 2advanced-neo/src/App.jsx
+++ 2advanced-neo/src/App.jsx
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import QuantumNetwork from './components/QuantumNetwork'; // Novo componente
+//import HolographicGlobalThreatMap from './components/HolographicGlobalThreatMap';
 import QuantumEncryptionVault from './components/QuantumEncryptionVault';
+import HolographicAIChatbot from './components/HolographicAIChatbot';
 import SystemStatus from './components/SystemStatus';
 import CyberThreats from './components/CyberThreats';
 import CommandConsole from './components/CommandConsole';
@@ -24,7 +26,9 @@ export default function App() {
         <Hero />
         <Features />
         <QuantumNetwork /> {/* Adicionado aqui */}
+        {/* <HolographicGlobalThreatMap /> ocultado tela branca*/}
         <QuantumEncryptionVault />
+        <HolographicAIChatbot />
         <SatelliteControl />
         <SystemStatus />
         <CyberThreats />
